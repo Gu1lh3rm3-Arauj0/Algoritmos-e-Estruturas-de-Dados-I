b53f5e0eec9707bbaf60b172ac86dab57ad82a7d
Guilherme Araújo <guilherme.a.m.souza0613@gmail.com>
2023-06-26 19:36:47 -0300

    Update a.c

--- a.c
+++ a.c
@@ -1,95 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define true 1
-#define fase 0
-typedef int bool;
-typedef int TIPOPESO;
+#include <string.h>
 
-typedef struct adjacencia {
-    int vertice; // vertice de destino
-    TIPOPESO peso; // peso associado a aresta que leva ao vertice de destino
-    struct adjacencia *prox; // O próximo elemento da lista de adjacencias
-}ADJACENCIA;
+struct cel {
+    int chave;
+    char letra;
+    struct cel* esq;
+    struct cel* dir;
+};
+typedef struct cel no;
 
-typedef struct vertice {
-    /* Dados armazenados vão aqui */
-    ADJACENCIA *cab; //possui apenas a cabeça da lista de adjacencia (linha 16)
-}VERTICE;
+typedef no* arvore;
 
-typedef struct grafo {
-    int vertice; // numero de vertice total do grafo     /*Apesar de não ser nescessario guardar o numero de vertices  e arestas, o fazemos, pois caso quisermos*/
-    int arestas; // numero de arestas totais do grafo  	/*saber desta informação, teriamos que percorrer toda a estrutura. Parece melhor perder 8bits de memória salvando isto ;)*/
-    VERTICE *adj; // Arranjo de vertices da estrutura (linha 22)
-}GRAFO;
-
-void GRAPHcptD1(GRAFO G, VERTICE s, VERTICE *pa, int *dist){
-
-   bool mature[1000];
-   for (VERTICE v = 0; v < G->V; ++v)
-      pa[v] = -1, mature[v] = false, dist[v] = INT_MAX;
-   pa[s] = s, dist[s] = 0;
-
-   while (true) {
-      // escolha de y:
-      int min = INT_MAX;
-      vertex y;
-      for (vertex z = 0; z < G->V; ++z) {
-         if (mature[z]) continue;
-         if (dist[z] < min)
-            min = dist[z], y = z;
-      }
-      if (min == INT_MAX) break;
-      // atualização de dist[] e pa[]:
-      for (link a = G->adj[y]; a != NULL; a = a->next) {
-         if (mature[a->w]) continue;
-         if (dist[y] + a->c < dist[a->w]) {
-            dist[a->w] = dist[y] + a->c;
-            pa[a->w] = y;
-         }
-      }
-
-      mature[y] = true;
-   }
+arvore Inicia() {
+    return NULL;
 }
 
-int main()
-{
-   int nc, nr, rotas;
-   int A, B;
-   int C, R, E;
-   int grafo[0][0];
-   //nc Arraestas (1h) do grafo e nr os vertices
+void Erd(arvore r, char help) {  // infixa
+    if (r != NULL) {
+        Erd(r->esq, help);
+         if(r->letra == help){
+        	 printf("%c", r->letra);
+         }
+          else{
+        	  printf(" %c", r->letra);
+          }
+        Erd(r->dir, help);
+    }
+}
 
-   while (getchar()!= EOF) {
-	   scanf("%d", &nc);
-	   scanf("%d", &nr);
 
-	   grafo[nr][nr];
-	   for(int i = 0; i < nc; i++){
-		   for(int j = 0; j < nc; j++){
-			   grafo[i][j]=0;
-		   }
-	   }
+void Red(arvore r, char help) {  // prefixa
+    if (r != NULL) {
+    	if(r->letra == help){
+    		printf("%c", r->letra);
+    	}
+    	else{
+    		printf(" %c", r->letra);
+    	}
+        Red(r->esq, help);
+        Red(r->dir, help);
+    }
+}
 
-	   for(int i = 0; i < nc; i++){
-		   scanf("%d", &A);
-		   scanf("%d", &B);
-		   grafo[A][B]=1;
-		   grafo[B][A]=1;
-	   }
+void Edr(arvore r, char help) {  // posfixa
+    if (r != NULL) {
+        Edr(r->esq, help);
+        Edr(r->dir, help);
+        if (r->letra == help) {
+            printf("%c", r->letra);
+        } else {
+            printf("%c ", r->letra);
+        }
+    }
+}
 
-	   scanf("%d %d %d", &C, &R, &E);
-	   for(int j = 0; j < nc; j++){
-		   grafo[E][j]=0;
-	   }
-	   for(int i = 0; i < nc; i++){
-		   grafo[i][E]=0;
-	   	}
-   }
+arvore Insere(arvore r, char letra) {
+    if (r == NULL) {
+        no* novo = malloc(sizeof(no));
+        novo->letra = letra;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        return novo;
+    }
 
-   int origem = C; // Vértice de origem
+    if (letra < r->letra) {
+        r->esq = Insere(r->esq, letra);
+    } else if (letra > r->letra) {
+        r->dir = Insere(r->dir, letra);
+    }
 
-   funçãoa(nr, grafo, origem);
+    return r;
+}
 
-   return 0;
+no* Busca(arvore r, char letra) {
+    if (r == NULL || r->letra == letra) {
+        return r;
+    }
+    if (letra < r->letra) {
+        return Busca(r->esq, letra);
+    } else {
+        return Busca(r->dir, letra);
+    }
+}
+
+int main() {
+    char op[10];
+    char elemento, help1, help2;
+    arvore arv, aux;
+
+    arv = Inicia();
+
+    while (scanf("%s", op) != EOF) {
+    	aux = arv;
+
+        if (strcmp(op, "I") == 0) {
+            scanf(" %c", &elemento);
+            arv = Insere(arv, elemento);
+        }
+        help1= arv->letra;
+
+        if (strcmp(op, "P") == 0) {
+            scanf(" %c", &elemento);
+            if (Busca(arv, elemento) != NULL) {
+            	printf("%c existe\n", elemento);
+            } else {
+                printf("%c nao existe\n", elemento);
+            }
+        }
+
+        if (strcmp(op, "INFIXA") == 0) {
+            aux = arv;
+            while (aux->esq != NULL) {
+                aux = aux->esq;
+            }
+            help2 = aux->letra;
+            Erd(arv, help2);
+            printf("\n");
+        }
+
+        if (strcmp(op, "PREFIXA") == 0) {
+            Red(arv, help1);
+            printf("\n");
+        }
+
+        if (strcmp(op, "POSFIXA") == 0) {
+            Edr(arv, help1);
+            printf("\n");
+        }
+    }
+
+    return 0;
 }
